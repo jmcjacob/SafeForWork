fdcaf95378570ba3bffca04ee3076575278d38b1
Jacob Carse <jacob.carse@gmail.com>
2015-10-30 13:45:52 +0000

    Connection to the Server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
 
 public class Main {
 
@@ -11,6 +13,17 @@ public class Main {
         {
             Socket clientSocket = new Socket("192.168.0.48", 5000);
 
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+            if (clientSocket.isConnected())
+            {
+                System.out.println("Connected");
+                out.println("Test");
+                out.flush();
+                out.close();
+            }
+            clientSocket.close();
         }
         catch (Exception exception)
         {
