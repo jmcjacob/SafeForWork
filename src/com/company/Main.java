f6bb278f53a441471b2893606458b001e6f832e9
Jacob Carse <jacob.carse@gmail.com>
2015-10-30 13:25:04 +0000

    Added Socket and basic Exceptions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        try
+        {
+            Socket clientSocket = new Socket("192.168.0.48", 5000);
+
+        }
+        catch (Exception exception)
+        {
+            System.out.println("ERROR: " + exception);
+        }
     }
 }
