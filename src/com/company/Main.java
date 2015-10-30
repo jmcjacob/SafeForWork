8bf81c311c833bee86d272a4acbe062492ff749e
Jacob Carse <jacob.carse@gmail.com>
2015-10-30 14:51:12 +0000

    Response from Server

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
+import java.nio.CharBuffer;
 
 public class Main {
 
@@ -12,17 +13,24 @@ public class Main {
         try
         {
             Socket clientSocket = new Socket("192.168.0.48", 5000);
-
+            ServerSocket socket = new ServerSocket(5000);
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader response = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            if (clientSocket.isConnected())
+            if (clientSocket.isConnected() )
             {
                 System.out.println("Connected");
-                out.println("Test");
+                System.out.println("HeRE");
+                out.println("HELO");
+                System.out.println("echo: " + response.readLine());
                 out.flush();
+                response.close();
                 out.close();
             }
+            else
+                System.out.println("ERROR");
+
+            System.out.println("Eended");
             clientSocket.close();
         }
         catch (Exception exception)
