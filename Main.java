c5d3e0df548c39b323c62885af88c6c0c48f07c3
Jacob Carse <jacob.carse@gmail.com>
2015-12-11 22:10:51 +0000

    Changed IP

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        if (Facade.initiate("192.168.0.48")) {
+        if (Facade.initiate("192.168.0.5")) {
             Facade.displayMoney();
             Facade.buy(Facade.clientSocket, "AVIVA", 10);
             Facade.displayMoney();
