bcd05ff3230358a1563d9ef2ad5a87332e05140e
Mitchell Bellamy <13374080@students.lincoln.ac.uk>
2015-12-12 02:08:57 +0000

    Add UI?

--- Main.java
+++ Main.java
@@ -3,18 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        if (Facade.initiate("192.168.0.48")) {
-            Facade.displayMoney();
-            Facade.buy(Facade.clientSocket, "AVIVA", 10);
-            Facade.displayMoney();
-            Facade.displayOwned();
-            Facade.sell(Facade.clientSocket, "AVIVA", 4);
-            Facade.displayMoney();
-            Facade.displayOwned();
-            Facade.exit(Facade.clientSocket);
+      String ip = "192.168.0.48"; //Default for Lab B Machines
+
+      ip = UserInterface.prompt("Specify the IP address of the server:");
+
+      if (Facade.initiate(ip)) {
+        UserInterface.menu();
+
         }
         else{
-            System.out.println("FAILED");
+            System.out.println("FAILED TO CONNECT TO IP: " + ip);
         }
     }
 }
